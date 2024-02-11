3d3e32aab3dc456615962e8046e73cf2233e5935
Eric Henderson <henderea@gmail.com>
2024-02-11 11:37:03 -0500

    initial version

---
+++ ts-types/global.d.ts
@@ -0,0 +1,15 @@
+declare type Optional<T> = T | null | undefined;
+declare type List<T> = ArrayLike<T>;
+declare type Many<T> = T | ReadonlyArray<T>;
+
+declare interface Dictionary<T> {
+  [index: string]: T;
+}
+
+declare let process: {
+  env: {
+    NODE_ENV: 'development' | 'production',
+    VERCEL_URL: Optional<string>;
+    BUILD_TIME: `${number}`;
+  }
+};
