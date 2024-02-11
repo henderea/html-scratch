771ec5c01d63b119e1183ef068481c4c60bd9c6e
Eric Henderson <henderea@gmail.com>
2024-02-11 12:32:50 -0500

    get the stuff working; note: typescript needs to compile to ES6

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
 import './index.scss';
-import $ from 'jquery';
+// import '@webcomponents/custom-elements';
+// import '@webcomponents/webcomponentsjs/webcomponents-bundle';
+// import $ from 'jquery';
 //import 'jquery-ui';
 //import _ from 'lodash';
 //import rand from 'lodash/rand';
 
 // @ts-ignore
-import registerServiceWorker from '@henderea/static-site-builder/registerServiceWorker';
+// import registerServiceWorker from '@henderea/static-site-builder/registerServiceWorker';
 
-if(process.env.NODE_ENV === 'production') {
-  registerServiceWorker();
+// if(process.env.NODE_ENV === 'production') {
+//   registerServiceWorker();
+// }
+
+const timezone = 'US/Eastern';
+
+class UserTz extends HTMLElement {
+  constructor() { super(); }
+
+  connectedCallback() {
+    this.textContent = timezone || 'US/Eastern';
+  }
+
+  get timezone(): string { return this.textContent || 'US/Eastern'; }
+  set timezone(value: string) { this.textContent = value || 'US/Eastern'; }
 }
+
+window.customElements.define('user-tz', UserTz);
